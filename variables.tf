7f94e777183fde3f703a7c2f5f0506d5091948c3
unknown <selbiger10@gmail.com>
2021-12-31 15:17:56 +0200

    Removed a bracket

--- variables.tf
+++ variables.tf
@@ -91,9 +91,10 @@ variable "instance_profile_name" {
 variable "server_key" {
   type        = string
   description = "Public SSH key name"
-
-  variable "elb_account_id" {
-    description = "ELB Account ID - pick one according to region https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html#access-logging-bucket-permissions"
-    type        = string
-  }
 }
+
+variable "elb_account_id" {
+  description = "ELB Account ID - pick one according to region https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html#access-logging-bucket-permissions"
+  type        = string
+}
+
